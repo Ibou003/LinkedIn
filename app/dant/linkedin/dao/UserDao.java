f0ed584868434250739c1709793cc76c7152a098
PatpatP <pol.patrick1411@gmail.com>
2015-05-17 13:39:57 +0200

    Commit

--- app/dant/linkedin/dao/UserDao.java
+++ app/dant/linkedin/dao/UserDao.java
@@ -1,10 +1,8 @@
 package dant.linkedin.dao;
 
-import java.util.List;
+
 
 import play.db.jpa.JPA;
-import dant.linkedin.core.Company;
-import dant.linkedin.core.Establishment;
 import dant.linkedin.core.User;
 
 public class UserDao {
